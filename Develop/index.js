878e82d072d9ece0855bb42b08daae8a9a5e0421
VZ13 <vinicius.zachary@gmail.com>
2024-03-03 10:12:41 -0700

    installed inquirer, added questions, and connected answers to generator function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        name:'title',
+        message:'What is your project called?',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Please enter a brief description.\nYour description should answer the following questions:\nWhy did you make this project?(not "because it was an assignment")\nWhat problem does this project solve?\nWhat did you learn in the making of this project?',
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What are the steps to install your project? (or N/A)',
+    },
+    {
+        type:'input',
+        name:'screenshot',
+        message:'Please provide the relative path to your screenshot',
+    },
+    {
+        type:'input',
+        name:'link',
+        message:'Please provide the URL to your deployed project',
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'What steps do users need to follow to fully utilize your project',
+    },
+    {
+        type:'input',
+        name:'contributions',
+        message:'Who contributed and what did they contribute?(or N/A)',
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'Please select an applicable license',
+        choices: ['MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'None']
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('README.md', generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None'){
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {return ''}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseURLs = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'APACHE2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'Boost1.0': 'https://opensource.org/licenses/BSL-1.0',
+   'MPL2.0': 'https://opensource.org/licenses/MPL-2.0',
+    'BSD2': 'https://opensource.org/licenses/BSD-2-Clause',
+    'BSD3': 'https://opensource.org/licenses/BSD-3-Clause',
+    // Add more licenses and their URLs as needed
+  };
+  if (license !== 'None'){
+    return `${licenseURLs[license]}`;
+  } else {return ''}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None'){
+    return `## License
+
+${license} License
+${renderLicenseBadge(license)}
+${renderLicenseLink(license)}`
+  } else {return ''}
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Screenshot
+
+![Project Screenshot](${data.screenshot})
+### deployed page --> ${data.link}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributions
+
+${data.contributions}
 `;
 }
 
